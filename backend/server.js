bf8dd39a49bcc5d582383f9ea65f4cc592aaa09d
AbukDuot <a.duot@alustudent.com>
2025-11-16 16:09:12 +0200

    update origin

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ const app = express();
 app.set('trust proxy', 1);
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'https://bridgeher.vercel.app'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'https://bridgeher.vercel.app', /https:\/\/bridgeher-.*\.vercel\.app$/],
   credentials: true
 }));
 app.use(express.json({ limit: '50mb' }));
